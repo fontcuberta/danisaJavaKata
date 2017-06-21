848619a414f5dbbd24a8bb39d3ef016f27d646bc
carlosble <carlos@codesai.com>
2017-06-22 00:26:52 +0200

    add exercise - value and reference

---
+++ test/YouShouldUnderstandValuesAndReferences.java
@@ -0,0 +1,31 @@
+import narrowOrParallel.valueAndReference.SomeType;
+import org.junit.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class YouShouldUnderstandValuesAndReferences {
+    @Test
+    public void because_its_fundamental() throws Exception {
+        SomeType instance = new SomeType();
+
+        String arg1 = "bar";
+        instance.firstMethod(arg1);
+        //assertThat(arg1).isEqualTo("???");
+
+        SomeType other = new SomeType();
+        other.numbers[0] = 100;
+        instance.secondMethod(other);
+        //assertThat(other.numbers[0]).isEqualTo(-1000);
+
+        SomeType another = new SomeType();
+        another.numbers[0] = 200;
+        instance.thirdMethod(another);
+        //assertThat(another.numbers[0]).isEqualTo(-1000);
+        //assertThat(another.child).isNull();
+
+        SomeType yetAnother = new SomeType();
+        yetAnother.numbers[0] = 200;
+        instance.fourthMethod(yetAnother.numbers);
+        //assertThat(yetAnother.numbers[0]).isEqualTo(-1000);
+    }
+}
